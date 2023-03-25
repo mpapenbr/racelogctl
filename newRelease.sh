87cda28a417e6fcc14b46b6c622aa78a4d065544
mpapenbr <Markus.Papenbrock+github@gmail.com>
2023-03-25 10:59:45 +0000

    chg: no local docker build

--- newRelease.sh
+++ newRelease.sh
@@ -12,7 +12,7 @@ echo $NO_PREFIX_TAG
 
 # updating the version in cmd/version.go
 go test ./... && \
-goreleaser --snapshot --skip-publish --rm-dist && \
+goreleaser --snapshot --skip-publish --clean --skip-docker && \
 sed -i -E "s/(Version\W+=\W*)\"(.*?)\"/\1\"$NO_PREFIX_TAG\"/" cmd/version.go && \
 git add cmd/version.go && git commit -m "chore: Release $FULL_TAG" && \
 git tag -a $FULL_TAG -m "Release $FULL_TAG" && \
